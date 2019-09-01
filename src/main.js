9f3068141b80cf161d1de8a7551f9331c5ff5f06
Michał Kowalski <michal.kowalski.wro@gmail.com>
2019-09-01 11:30:32 +0200

    icons from font-awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import firebase from "firebase";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUserSecret,
+  faEdit,
+  faAdjust,
+  faAlignCenter,
+  faRedo,
+  faPlay
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
+
+library.add(faUserSecret, faEdit, faAdjust, faAlignCenter, faRedo, faPlay);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 const config = {
   apiKey: "AIzaSyBG95iixK_JyLw5kfhU5OwgJuHyZYPAgPU",
